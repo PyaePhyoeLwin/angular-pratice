e79b4a7333ff55d14ab15b8c08cc5ce82586d07b
PyaePhyoeLwin <pyaephyoelwin1@gmail.com>
2022-04-20 00:15:28 +0900

    for pratice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,26 +7,25 @@ interface TestData {
   price: number;
 }
 
+enum ProductCategoryEnum {
+  SHOES = 'shoes',
+  CLOTHES = 'clothes',
+  SUNGLASS = 'sunglass',
+}
+
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  // public shoeShowName: string = 'shoes';
-  public value: string = 'null';
+  public readonly productCategoryEnum = ProductCategoryEnum;
+  public readonly productCategories: ProductCategoryEnum[] = [
+    ProductCategoryEnum.SHOES,
+    ProductCategoryEnum.CLOTHES,
+    ProductCategoryEnum.SUNGLASS,
+  ];
 
-  // public showShoesName(): void {
-  //   this.shoeShowName;
-  //   console.log(this.shoeShowName);
-  // }
-
-  public toggleAll(): void {
-    console.log('value:' + this.value);
-    // if (this.value === 'shoes') {
-    //   this.showShoesName();
-    // }
-  }
   products: TestData[] = [
     { id: 1, name: 'adidas', code_num: 'S111', category: 'shoes', price: 5000 },
     { id: 2, name: 'nike', code_num: 'S123', category: 'shoes', price: 4000 },
@@ -46,4 +45,32 @@ export class AppComponent {
       price: 5000,
     },
   ];
+
+  public filteredProducts: TestData[] = this.products;
+
+  // public shoeShowName: string = 'shoes';
+  public value: string = 'null';
+
+  // public showShoesName(): void {
+  //   this.shoeShowName;
+  //   console.log(this.shoeShowName);
+  // }
+
+  public toggleAll(): void {
+    console.log('value:' + this.value);
+    // if (this.value === 'shoes') {
+    //   this.showShoesName();
+    // }
+  }
+
+  public filterProductCategory(): void {
+    if (!this.value) {
+      this.filteredProducts = this.products;
+      return;
+    }
+
+    this.filteredProducts = this.products.filter(
+      ({ category }) => category === this.value
+    );
+  }
 }
