a19c2b86a74dab4fed91e7fb82a2e5d6cd6aa1d5
PyaePhyoeLwin <pyaephyoelwin1@gmail.com>
2022-04-20 01:25:24 +0900

    efefe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HighlightDirective } from './highlight.directive';
 // import { HelloComponent } from './hello.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent],
+  declarations: [ AppComponent, HighlightDirective],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
